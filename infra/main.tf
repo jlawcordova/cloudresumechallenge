add1de0d5ce510189952dab031f833c3f1040f02
J. Law. Cordova <jlaw.cordova@gmail.com>
2023-04-18 10:02:56 +0800

    ci: create dynamoDB table

---
+++ infra/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.4.0"
+    }
+  }
+  required_version = "~> 1.2"
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_dynamodb_table" "view-count-table" {
+  name           = "ViewCount"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "Id"
+
+  attribute {
+    name = "Id"
+    type = "S"
+  }
+
+  tags = {
+    Project     = "development"
+    Environment = "cloudresumechallenge"
+  }
+}
+
