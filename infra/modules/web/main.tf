d9e509e13f6170d0f57b319a7546f6bc083c0250
J. Law. Cordova <jlaw.cordova@gmail.com>
2023-05-20 11:51:01 +0800

    ci: create web module

--- infra/modules/web/main.tf
+++ infra/modules/web/main.tf
@@ -57,3 +57,46 @@ resource "aws_s3_object" "web" {
   # MD5 hash of that object.
   etag = each.value.digests.md5
 }
+
+# Setup the CloudFront distribution.
+resource "aws_cloudfront_distribution" "web" {
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.web.website_endpoint
+    origin_id   = aws_s3_bucket_website_configuration.web.website_endpoint
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
+  enabled     = true
+  price_class = "PriceClass_100"
+
+  default_cache_behavior {
+    # Using the CachingOptimized managed policy ID
+    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = aws_s3_bucket_website_configuration.web.website_endpoint
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    Project     = var.project
+    Environment = var.environment
+  }
+}
