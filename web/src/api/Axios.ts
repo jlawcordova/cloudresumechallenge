2ce7ee7593c2b2a48b2dd10e1e460985265c28b6
J. Law. Cordova <jlaw.cordova@gmail.com>
2023-05-20 12:40:56 +0800

    fix: use env vars to set the base URL (#7)

--- web/src/api/Axios.ts
+++ web/src/api/Axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosClient = axios.create({
-  baseURL: "https://api-resume.jlawcordova.com/",
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
