52ab6c87620d6ab8970577d88a8b13f25c64496f
J. Law. Cordova <jlaw.cordova@gmail.com>
2023-07-12 20:21:32 +0800

    ci: deploy web prod on main (#15)

--- infra/modules/app/output.tf
+++ infra/modules/app/output.tf
@@ -6,5 +6,5 @@ output "project" {
 output "url" {
   description = "The URL for Cloud Resume Challenge API Gateway."
 
-  value = coalesce(var.domain, aws_apigatewayv2_stage.default.invoke_url)
+  value = var.domain == null || var.domain == "" ? aws_apigatewayv2_stage.default.invoke_url : "https://" + var.domain + "/"
 }
--- infra/modules/web/variables.tf
+++ infra/modules/web/variables.tf
@@ -3,6 +3,13 @@ variable "project" {
   description = "The name of the project. This is used for resource naming and tagging."
 }
 
+variable "domain" {
+  type        = string
+  default     = null
+  description = "The domain for the web."
+  nullable    = true
+}
+
 variable "environment" {
   type = string
   default = "development"
