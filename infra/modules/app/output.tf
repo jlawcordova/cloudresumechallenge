53636df63250ac85ef73ab9159c73d7f51764e9e
J. Law. Cordova <jlaw.cordova@gmail.com>
2023-07-12 20:32:37 +0800

    fix: concat domains correctly (#16)

--- infra/modules/app/output.tf
+++ infra/modules/app/output.tf
@@ -6,5 +6,5 @@ output "project" {
 output "url" {
   description = "The URL for Cloud Resume Challenge API Gateway."
 
-  value = var.domain == null || var.domain == "" ? aws_apigatewayv2_stage.default.invoke_url : "https://" + var.domain + "/"
+  value = var.domain == null || var.domain == "" ? aws_apigatewayv2_stage.default.invoke_url : "https://${var.domain}/"
 }
